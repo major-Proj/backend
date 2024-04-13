f65e3cb8d8829317c9d2442f67399226e9c6dbee
Arjun P <72484657+arjunprakash027@users.noreply.github.com>
2024-04-13 12:16:11 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const conn_str = "mongodb+srv://aiarjun027:arjun1234@cluster0.beh4ixw.mongodb.net/timesheet?retryWrites=true&w=majority"
+const conn_str = "mongodb+srv://aiarjun027"
 mongoose.connect(conn_str).then(() => console.log("Mongo connection Successsfully")).catch((err) => console.log(err))
 module.exports = {
     mongoose
